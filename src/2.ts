ba704803ec31cec6ad9a1a6e946dba30725f6dde
AIex43 <129332589+AIex43@users.noreply.github.com>
2025-06-16 11:36:37 +0300

    Update 2.ts

--- src/2.ts
+++ src/2.ts
@@ -1,24 +1,7 @@
-/*
-  Ваше завдання полягатиме у створенні двох класів – Employee та Manager.
-
-  Клас Employee повинен включати:
-
-  властивість name, яка буде доступна всім.
-  властивість department, яка буде доступна лише всередині класу Employee.
-  salary, яке буде доступне лише всередині класу Employee та його підкласів.
-
-
-  Клас Manager повинен бути підклас класу Employee
-
-  Необхідно реалізувати в класі Manager конструктор, який викликатиме конструктор суперкласу та збільшуватиме salary на 10000.
-
-*/
-
 class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  public name: string;
+  private department: string;
+  protected salary: number;
 
   constructor(name: string, department: string, salary: number) {
     this.name = name;
@@ -26,14 +9,24 @@ class Employee {
     this.salary = salary;
   }
 
-  getEmployeeDetails() {
+  public getEmployeeDetails(): string {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
   }
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary);
+    this.salary += 10000;
+  }
+
+  public getManagerDetails(): string {
+    return `Manager ${this.name}, Salary: ${this.salary}`;
+  }
 }
 
+const e = new Employee("Olena", "HR", 50000);
+console.log(e.getEmployeeDetails());
 
-export {};
+const m = new Manager("Taras", "IT", 70000);
+console.log(m.getEmployeeDetails());
